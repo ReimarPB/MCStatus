ef0cc0d36f64465f2a29d4ea140bc8ecbac87519
ReimarPB <reimarpb@gmail.com>
2022-05-04 17:16:53 +0200

    Switch to CMake

--- build.bat
+++
@@ -1,9 +0,0 @@
-:: Requires you to add C:\Program Files (x86)\Microsoft Visual Studio\*\*\VC to PATH
-@echo off
-setlocal
-mkdir out 2> nul
-cd out
-call vcvarsall %PROCESSOR_ARCHITECTURE%
-cl ../mcstatus.c ../errors.c ../protocols/java.c ../protocols/legacy_java.c ../protocols/tcp.c ../protocols/chat_parser.c ../lib/cJSON/cJSON.c ../lib/cJSON/cJSON_Utils.c
-cd ..
-endlocal
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p out
-cd out
-$CC ../mcstatus.c ../errors.c ../protocols/java.c ../protocols/legacy_java.c ../protocols/tcp.c ../protocols/chat_parser.c ../lib/cJSON/cJSON.c ../lib/cJSON/cJSON_Utils.c -o mcstatus
-cd ..
-
